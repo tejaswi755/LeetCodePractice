41bf3fc457571cee745f74543389de96dcb5e7a5
Tejaswi Sharma <75516011+tejaswi755@users.noreply.github.com>
2023-03-09 20:30:20 +0530

    Time: 23 ms (15.15%), Space: 13.4 MB (39.21%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -7,25 +7,45 @@ public:
        
         int size=matrix.size();
         int size1=matrix[0].size();
-        
+        bool iscol=false;
+        int flag=matrix[0][0];
         for(int i=0;i<size;i++){
-            for(int j=0;j<size1;j++){
+            if(matrix[i][0]==0){
+                iscol=true;
+            }
+            for(int j=1;j<size1;j++){
                 if(matrix[i][j]==0){
-                    r[i]=true;
-                    col[j]=true;
+                   matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
         
       
-        for(int i=0;i<size;i++){
-            for(int j=0;j<size1;j++){
-                if(col[j]==true){
+        for(int i=1;i<size;i++){
+            for(int j=1;j<size1;j++){
+                if(matrix[i][0]==0||matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
-                if(r[i]==true){
-                    matrix[i][j]=0;
-                }
+            }
+        }
+        
+   
+        if(matrix[0][0]==0){
+           
+            for(int i=0;i<size1;i++){
+               // cout<<matrix[0][i]<<" ";
+                matrix[0][i]=0;
+             
+                //cout<<matrix[0][i]<<endl;
+            }
+            
+        }
+        
+        if(iscol){
+            for(int i=0;i<size;i++){
+               
+                matrix[i][0]=0;
                 
             }
         }
