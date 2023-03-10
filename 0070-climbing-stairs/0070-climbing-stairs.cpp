a8f917c21a8b0c8537217dcc5c268ccddd644415
Tejaswi Sharma <75516011+tejaswi755@users.noreply.github.com>
2023-03-10 20:22:24 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (19.70%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,34 +2,34 @@ class Solution {
 public:
     
    
-//     int solve(int n,vector<int>dp){
-//         if(n==1){
-//             return 1;
-//         }
-//         if(n==2){
-//             return 2;
-            
-//         }
-//         if(dp[n]!=-1){
-//             return dp[n];
-//         }
-        
-//       dp[n]= solve(n-1,dp)+solve(n-2,dp);
-//         return dp[n];
-//     }
-    int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
+    int solve(int n,vector<int>&dp){
         if(n==1){
             return 1;
         }
-        dp[1]=1;
- 
-        dp[2]=2;
-        for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        if(n==2){
+            return 2;
+            
+        }
+        if(dp[n]!=-1){
+            return dp[n];
         }
         
+      dp[n]= solve(n-1,dp)+solve(n-2,dp);
         return dp[n];
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+//         if(n==1){
+//             return 1;
+//         }
+//         dp[1]=1;
+ 
+//         dp[2]=2;
+//         for(int i=3;i<=n;i++){
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+        
+        return solve(n,dp);
         
     }
 };
