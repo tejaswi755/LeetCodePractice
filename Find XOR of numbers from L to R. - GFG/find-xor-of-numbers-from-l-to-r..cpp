0b37ce32ec2193f713144608e7a5b4370a3b5c8e
Tejaswi Sharma <75516011+tejaswi755@users.noreply.github.com>
2023-04-05 09:47:28 +0530

    Added solution - LeetHub

---
+++ Find XOR of numbers from L to R. - GFG/find-xor-of-numbers-from-l-to-r..cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+  int solve(int n){
+      if(n%4==0){
+          return n;
+      }else if(n%4==1){
+          return 1;
+      }else if(n%4==2){
+          return (n+1);
+      }else {
+          return 0;
+      }
+  }
+    int findXOR(int l, int r) {
+        // complete the function here
+     
+        return solve(l-1)^solve(r);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        // Input
+
+        int l, r;
+        cin >> l >> r;
+
+        Solution obj;
+        cout << obj.findXOR(l, r) << endl;
+    }
+}
+// } Driver Code Ends
